fe01badaa00492b1b9273f922259cb7b98bb5e91
Dreempreter <dreempreter@gmail.com>
2023-03-04 20:54:03 -0500

    First Commit

---
+++ src/com/enes/murad/MuradEnes.java
@@ -0,0 +1,12 @@
+package com.enes.murad;
+
+public class MuradEnes {
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Dreem
+ *
+ */
+module MuradEnes {
+}
